946ca8db154f192c77000dd43cfa852222073676
MaxAtilla24 <maxatilla24@gmail.com>
2023-05-12 13:25:05 -0500

    working version with include()

--- Mercadona/urls.py
+++ Mercadona/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls'))
+    path('', include('product.urls')),
     
     
 ]
